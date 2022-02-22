315967eee586d949a52f74b71c3e7c280d20448d
blue spinny <honerjerome@gmail.com>
2022-02-22 16:35:52 +0100

    dictonarys

---
+++ index.py
@@ -0,0 +1,16 @@
+var1 = 1
+var2 = 2
+
+yes = False
+index = {
+    
+    "hi" : var1,
+    "hi1" : var2
+}
+
+
+if yes == True:
+    
+    print(index["hi" + str(var1)])
+else:
+    print(index["hi"])
